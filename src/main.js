bbec549e2b00808430421bcbde428a432199da3d
Shawn Truesdale <shawnctruesdale@gmail.com>
2020-09-03 19:44:15 -0600

    Complete JS functionality, basics

---
+++ src/main.js
@@ -0,0 +1,33 @@
+var crockpot = document.querySelector('.crockpot');
+var recipeText = document.querySelector('.recipe-text')
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function addRecipe() {
+
+}
+
+function letsCook() {
+  var checkedButton = document.querySelector('input[name="meal"]:checked').value;
+  if (checkedButton === 'side') displayFood(randomFood(sides));
+  if (checkedButton === 'main') displayFood(randomFood(mains));
+  if (checkedButton === 'dessert') displayFood(randomFood(desserts));
+  if (checkedButton === 'entire') entireMeal();
+}
+
+function randomFood(foodItem) {
+  return foodItem[getRandomIndex(foodItem)];
+}
+
+function entireMeal() {
+  var wholeMeal = `${randomFood(mains)} with a side of ${randomFood(sides)} and a ${randomFood(desserts)} for dessert!`;
+  displayFood(wholeMeal);
+}
+
+function displayFood(food) {
+  crockpot.classList.add('hidden');
+  recipeText.classList.remove('hidden');
+  recipeText.innerText = food;
+}
